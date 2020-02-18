fe9b1968a78256c2409cdd18b4b08fe6d5c8a882
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-02-18 09:04:26 +0100

    Use Julia project structure.

--- src/FiniteVolumes.jl
+++ src/FiniteVolumes.jl
@@ -1,3 +1,4 @@
+module FiniteVolumes
 
 include("./models/scalar_linear_advection.jl")
 include("./models/n_scalars_linear_advection.jl")
@@ -5,3 +6,5 @@ include("./models/isothermal_two_fluid.jl")
 include("./mesh.jl")
 include("./reconstruction.jl")
 include("./scheme.jl")
+
+end
