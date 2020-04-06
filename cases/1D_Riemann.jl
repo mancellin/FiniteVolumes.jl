c540f813a35a8232ee9f27565dab0ddc4374372b
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-04-06 17:34:05 +0200

    Fix 1D Riemann test case.

--- cases/1D_Riemann.jl
+++ cases/1D_Riemann.jl
@@ -1,6 +1,7 @@
 #!/usr/bin/env julia
 
-include("../src/finite_volumes.jl")
+using FiniteVolumes
+using StaticArrays
 
 grid = RegularMesh1D(0.0, 1.0, 100)
 model = IsothermalTwoFluidEuler{Float64, nb_dims(grid)}(300.0, 1.0, 1500.0, 1000.0, 1e5)
@@ -8,11 +9,8 @@ model = IsothermalTwoFluidEuler{Float64, nb_dims(grid)}(300.0, 1.0, 1500.0, 1000
 w₀ = [i < nb_cells(grid)/2 ? SVector(2e5, 0.0, 1.0) : SVector(1e5, 0.0, 0.0) for i in 1:nb_cells(grid)]
 
 w = deepcopy(w₀)
-wsupp = map(wi -> compute_wsupp(model, wi), w)
-run!(model, grid, w, wsupp, cfl=0.4, nb_time_steps=100)
+wsupp = map(wi -> FiniteVolumes.compute_wsupp(model, wi), w)
+FiniteVolumes.run!(model, grid, w, wsupp, cfl=0.4, nb_time_steps=100)
 
-using PyPlot
-figure()
-step([cell_center(grid, i) for i in 1:nb_cells(grid)], [wi[1] for wi in w₀])
-step([cell_center(grid, i) for i in 1:nb_cells(grid)], [wi[1] for wi in w])
-show()
+using Plots
+plot(grid, [w₀ w], 1, label=["initial" "final"])
