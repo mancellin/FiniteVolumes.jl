809143d14f7a0841e63a08c7257d8ed9819d5088
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-04-23 18:56:32 +0200

    Update other example cases.
    WIP: directional splitting is not supported for Euler at the moment.

--- cases/2D_advection.jl
+++ cases/2D_advection.jl
@@ -11,7 +11,7 @@ model = IsothermalTwoFluidEuler{Float64, nb_dims(grid)}(300.0, 1.0, 1500.0, 1000
 is_in_disk(i) = norm([0.5, 0.5] - cell_center(grid, i)) < 0.3
 
 w₀ = [SVector(1e5, 10.0, 10.0, is_in_disk(i) ? 0.0 : 1.0) for i in 1:nb_cells(grid)]
-t_final, w = FiniteVolumes.run(model, directional_splitting(grid), w₀, dt=3e-6, nb_time_steps=1_000)
+t_final, w = FiniteVolumes.run(model, grid, w₀, dt=3e-6, nb_time_steps=1_000)
 
 wsupp = (wi -> FiniteVolumes.compute_wsupp(model, wi)).(w)
 using Plots
